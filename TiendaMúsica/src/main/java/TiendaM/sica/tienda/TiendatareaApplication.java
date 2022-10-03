69c5b579c5b0300bf9f2184accfec637e6e7d7c5
Arielst11 <asancheztroz@gmail.com>
2022-10-02 21:29:20 -0600

    Commit de la tarea
    Tarea pagina completada

---
+++ TiendaMúsica/src/main/java/TiendaM/sica/tienda/TiendatareaApplication.java
@@ -0,0 +1,13 @@
+package TiendaM.sica.tienda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendatareaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendatareaApplication.class, args);
+	}
+
+}
---
+++ TiendaMúsica/src/test/java/TiendaM/sica/tienda/TiendatareaApplicationTests.java
@@ -0,0 +1,13 @@
+package TiendaM.sica.tienda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendatareaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
